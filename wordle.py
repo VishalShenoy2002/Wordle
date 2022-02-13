d8d8f244f6cd67604dca3f0ee9b9b8e6d01629a2
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2022-02-13 18:55:01 +0530

    Changed Word List and Modified main.py and wordle.py

--- wordle.py
+++ wordle.py
@@ -1,4 +1,5 @@
 from colorama import Fore
+from string import ascii_uppercase
 
 class Wordle:
 
@@ -11,14 +12,23 @@ class Wordle:
     IN_POSITION_COLOR=Fore.GREEN
     DEFAULT_COLOR=Fore.RESET
     CORRECT_WORD_COLOR=Fore.GREEN
+    AVAILABLE_LETTER_COLOR=Fore.WHITE
 
 
     def __init__(self,secret_word:str):
 
         self.word=""
         self.secret_word=secret_word.upper()
+        self.secret_word_count={}
+        
+
         self.attepmts=0
         self.words_attempted=["","","","","",""]
+
+        self.letters_in_positions_secret=[]
+        self.letters_in_secret=[]
+        self.letters_not_in_secret=[]
+        self.available_letters=[x for x in ascii_uppercase]
 
 
     @property
@@ -39,12 +49,12 @@ class Wordle:
             self.attepmts+=1
 
             if self.isSolved:
-                self.displayWordsGuessed()
                 self.SOLVED_BEFORE_ALL_ATTEPMTS=True
                 self.displayMessage("You have Guessed the Word Correctly in {} attempts.".format(self.attepmts),self.CORRECT_WORD_COLOR)
             
             else:
                 self.displayWordsGuessed()
+                self.displayColoredLetters()
 
                 
         else:
@@ -73,6 +83,23 @@ class Wordle:
         
         print("{}{}{}".format(color,message,self.DEFAULT_COLOR),end=end)
 
+    def displayColoredLetters(self):
+        print("\n")
+        letters=[]
+        for letter in self.available_letters:
+            if letter in self.letters_in_positions_secret:
+                letters.append("{}{}{}".format(self.IN_POSITION_COLOR,letter,self.DEFAULT_COLOR))
+
+            elif letter in self.letters_in_secret:
+                letters.append("{}{}{}".format(self.IN_WORD_COLOR,letter,self.DEFAULT_COLOR))
+
+            elif letter in self.letters_not_in_secret:
+                letters.append("{}{}{}".format(self.WARNING_COLOR,letter,self.DEFAULT_COLOR))
+            
+            else:
+                letters.append("{}{}{}".format(self.AVAILABLE_LETTER_COLOR,letter,self.DEFAULT_COLOR))
+
+        print("  ".join(letters))
 
     def checkResult(self,word:str):
         
@@ -82,12 +109,17 @@ class Wordle:
 
                 if  word[i] in self.secret_word[i]:
                     word_with_color.append(self.displayLetter("{}".format(word[i]),self.IN_POSITION_COLOR))
+                    self.letters_in_positions_secret.append(word[i])
+                
                 
                 elif word[i] in self.secret_word:
                     word_with_color.append(self.displayLetter("{}".format(word[i]),self.IN_WORD_COLOR))
+                    self.letters_in_secret.append(word[i])
 
                 else:
                     word_with_color.append(self.displayLetter("{}".format(word[i]),self.WARNING_COLOR))
+                    self.letters_not_in_secret.append(word[i])
+
             
             
             return "  ".join(word_with_color)
